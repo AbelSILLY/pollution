74357684e0d0d5264c19d8288174ee3ee7649981
TristanRivaldi <145669069+TristanRivaldi@users.noreply.github.com>
2023-11-24 13:44:19 +0100

    Update carteAlertes.py

--- carteAlertes.py
+++ carteAlertes.py
@@ -15,7 +15,7 @@ m = folium.Map(location=[43.6, 1.4], zoom_start=8)
 # Créer un dictionnaire pour stocker le nombre d'alertes et les dates associées à chaque point
 alerts_by_location = {}
 
-# Ajouter des marqueurs rouges pour chaque alerte avec le nombre d'alertes et toutes les dates
+# Ajouter des marqueurs rouges pour chaque alerte avec le nombre d'alertes, la date et le polluant responsable
 features = data.get('features', [])
 for feature in features:
     properties = feature.get('properties', {})
@@ -24,17 +24,18 @@ for feature in features:
     etat = properties.get('etat', '')
     date_ech = properties.get('date_ech', '')
     lib_zone = properties.get('lib_zone', '')
+    lib_pol = properties.get('lib_pol', '')  # Ajout du polluant responsable
 
     # Vérifier si l'état est une alerte et si la date est présente
     if etat == 'ALERTE' and date_ech:
         date_str = datetime.fromtimestamp(date_ech / 1000).strftime('%m/%d/%Y')
 
-        # Ajouter le nombre d'alertes et la date au dictionnaire associé aux coordonnées
+        # Ajouter le nombre d'alertes, la date et le polluant responsable au dictionnaire associé aux coordonnées
         if tuple(coordinates) in alerts_by_location:
             alerts_by_location[tuple(coordinates)]['count'] += 1
-            alerts_by_location[tuple(coordinates)]['dates'].append(date_str)
+            alerts_by_location[tuple(coordinates)]['dates'].append(f"{date_str} ({lib_pol})")
         else:
-            alerts_by_location[tuple(coordinates)] = {'count': 1, 'dates': [date_str], 'lib_zone': lib_zone}
+            alerts_by_location[tuple(coordinates)] = {'count': 1, 'dates': [f"{date_str} ({lib_pol})"], 'lib_zone': lib_zone}
 
 # Ajouter des marqueurs pour chaque point avec une taille de marqueur variable en fonction du nombre d'alertes
 for coordinates, alert_info in alerts_by_location.items():
@@ -50,15 +51,6 @@ for coordinates, alert_info in alerts_by_location.items():
     # Créer le marqueur avec une taille variable
     folium.CircleMarker(location=[coordinates[1], coordinates[0]], radius=radius, popup=popup_text, color='red', fill=True).add_to(m)
 
-# Enregistrer la carte sur le bureau avec le nom "carteA.html"
-desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-map_path = os.path.join(desktop_path, "carteA.html")
-
-# Enregistrer la carte sur le bureau
-m.save(map_path)
-
-# Afficher le chemin d'accès au fichier enregistré
-print(f"La carte a été enregistrée sur votre bureau : {map_path}")
-
 # Afficher la carte dans le notebook
 display(m)
+
