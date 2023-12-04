3e6363c6ffdc4d7c4e701132cccc7f12d4a89393
Abel SILLY <abel.silly@etu.umontpellier.fr>
2023-12-04 19:44:54 +0100

    ajout premières données météo

--- modpollution/__init__.py
+++ modpollution/__init__.py
@@ -2,4 +2,6 @@ __version__ = "0.0.1"
 
 from .io.Load import Load
 from .traitement.traitement import as_df
-from .traitement.traitement import extraire_donnees_pollution
+from .traitement.traitement import extraire_donnees_station
+from .traitement.traitement import extraire_polluant
+from .vis.affichage import plotpoll
