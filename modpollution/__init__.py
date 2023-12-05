ee801f0670aa2825708e21288fcccdbb3bf4e628
Abel SILLY <abel.silly@etu.umontpellier.fr>
2023-12-05 21:19:14 +0100

    update module

--- modpollution/__init__.py
+++ modpollution/__init__.py
@@ -5,4 +5,6 @@ from .traitement.traitement import as_df
 from .traitement.traitement import extraire_donnees_station
 from .traitement.traitement import extraire_polluant
 from .traitement.traitement import modif_date
+from .traitement.traitement import modif_date2
+
 from .vis.affichage import plotpoll
