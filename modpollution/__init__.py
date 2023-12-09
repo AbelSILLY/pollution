15d7cd9f3b8a0d43058e2d166dbaf10f20d5fce6
Abel SILLY <abel.silly@etu.umontpellier.fr>
2023-12-09 16:24:00 +0100

    petit changement relatif aux csv

--- modpollution/__init__.py
+++ modpollution/__init__.py
@@ -9,6 +9,7 @@ from .traitement.traitement import modif_date2
 from .traitement.traitement import extraire_donnees_villes
 from .traitement.traitement import mean_df
 from .traitement.traitement import modif_date_meteo
+from .traitement.traitement import modif_date_csv
 from .traitement.traitement import as_df_meteo
 from .vis.affichage import plotpoll
 from .vis.affichage import plotpollline
