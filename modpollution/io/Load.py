4c829454662317694b02a8eec4e75b5bd115b967
Abel SILLY <abel.silly@etu.umontpellier.fr>
2023-12-09 16:22:47 +0100

    ajout des fichiers csv

--- modpollution/io/Load.py
+++ modpollution/io/Load.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from modpollution.io import url_dm, url_da, url_j, url_meteo, path_target
+from modpollution.io import url_dm, url_da, url_dj, url_meteo, path_target, url_dah, url_dmcsv, url_dacsv,url_djcsv
 
 class Load:
     """
@@ -14,22 +14,38 @@ class Load:
     target_name : (string) chemin où on stock les données
     """
     
-    def __init__(self, url_m=url_dm,url_a=url_da,url_meteo=url_meteo,target_name=path_target):
+    def __init__(self,url_jcsv=url_djcsv,url_j=url_dj,url_mcsv=url_dmcsv, url_m=url_dm,url_a=url_da,url_acsv=url_dacsv,url_meteo=url_meteo,target_name=path_target,url_ah=url_dah):
         path = path_target
         fname_m = "data_m.json"
+        fname_mcsv = 'data_m.csv'
         fname_a = "data_a.json"
+        fname_acsv='data_a.csv'
         fname_j = "data_j.json"
+        fname_jcsv='data_j.csv'
+        fname_ah = 'data_ah.json'
         fname_meteo = "data_meteo.json"
         data_m = requests.get(url_m)
+        data_mcsv = requests.get(url_mcsv)
         data_a = requests.get(url_a)
+        data_acsv = requests.get(url_acsv)
         data_j = requests.get(url_j)
+        data_jcsv=requests.get(url_jcsv)
         meteo = requests.get(url_meteo)
+        data_ah = requests.get(url_ah)
         with open(os.path.join(path_target,fname_m),'w') as output_file:
             output_file.write(data_m.text)
+        with open(os.path.join(path_target,fname_mcsv),'w') as output_file:
+            output_file.write(data_mcsv.text)
         with open(os.path.join(path_target,fname_a),'w') as output_file:
             output_file.write(data_a.text)
+        with open(os.path.join(path_target,fname_acsv),'w') as output_file:
+            output_file.write(data_acsv.text)
         with open(os.path.join(path_target,fname_meteo),'w') as output_file:
             output_file.write(meteo.text)
         with open(os.path.join(path_target,fname_j),'w') as output_file:
             output_file.write(data_j.text)
+        with open(os.path.join(path_target,fname_jcsv),'w') as output_file:
+            output_file.write(data_jcsv.text)
+        with open(os.path.join(path_target,fname_ah),'w') as output_file:
+            output_file.write(data_ah.text)
     
--- modpollution/io/__init__.py
+++ modpollution/io/__init__.py
@@ -1,9 +1,12 @@
 import os
 url_dm = "https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_mensuelle_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+url_dmcsv= 'https://opendata.arcgis.com/api/v3/datasets/3acfa2aa5c0346a18ba7749c6885e503_0/downloads/data?format=csv&spatialRefId=4326&where=1%3D1'
 url_da = "https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_annuelle_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+url_dacsv = 'https://opendata.arcgis.com/api/v3/datasets/e0f26f918c504608b10d128f65ce0efc_0/downloads/data?format=csv&spatialRefId=4326&where=1%3D1'
 url_meteo = "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/donnees-synop-essentielles-omm/records?select=tc%2Cnom%2Cdate%2Ctemps_present&where=date%20%3E%3D%20date%272022%27%20AND%20nom_dept%3D%27H%C3%A9rault%27&order_by=date&limit=100"
-url_j = "https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_journaliere_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
-
+url_dj = "https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_journaliere_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json"
+url_djcsv = 'https://opendata.arcgis.com/api/v3/datasets/2ab16a5fb61f42c1a689fd9cc466383f_0/downloads/data?format=csv&spatialRefId=4326&where=1%3D1'
+url_dah = 'https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_72h_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
 path_target= os.path.join(
     os.path.dirname(os.path.realpath(__file__)),"..","data"
 )
