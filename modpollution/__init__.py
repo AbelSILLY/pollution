ba5e653ee8ae1970cdf336561ae0792fb30fe8a2
jeannevivierum <145753305+jeannevivierum@users.noreply.github.com>
2023-12-10 23:07:32 +0100

    ajout comparatifs

--- modpollution/__init__.py
+++ modpollution/__init__.py
@@ -17,6 +17,7 @@ from .traitement.traitement import mean_by_dep
 from .vis.affichage import plotpoll
 from .vis.affichage import plotpollline
 from .vis.affichage import plotmeteo
-from .vis.affichage import plotbis
+from .vis.affichage import plotville
+from .vis.affichage import plotdep
 from .vis.visu import horaire
 from .vis.visu import graph_horaire
